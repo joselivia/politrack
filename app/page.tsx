f01371379dec90e44bdb66f6ff0878e4cec25403
Blain Muema <blainmuema@gmail.com>
2025-09-30 01:30:05 +0300

    fix: Correct formatting in page.tsx and update baseUrl.ts for local development

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Header } from "@/components/header";
 import { HeroSection } from "@/components/hero-section";
@@ -7,16 +7,20 @@ import { InsightsSection } from "@/components/insights-section";
 import { CaseStudiesSection } from "@/components/case-studies-section";
 import { AboutSection } from "@/components/about-section";
 import { Footer } from "@/components/footer";
+import ToastCard from "@/components/toastNotification";
+import LiveDetailsReport from "@/components/reports/LiveDetailsReport";
 export default function Home() {
   return (
     <main>
       <Header />
       <HeroSection />
-     <ServicesSection />
+      <ServicesSection />
       <InsightsSection />
       <CaseStudiesSection />
+      <ToastCard />
+      <LiveDetailsReport />
       <AboutSection />
-      <Footer />  
+      <Footer />
     </main>
   );
 }
--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseURL = "https://politrackafrica.co.ke";
-//export const baseURL="http://localhost:8082";
+// export const baseURL = "https://politrackafrica.co.ke";
+export const baseURL = "http://localhost:8082";
