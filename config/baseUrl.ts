6f519634210ae2697c99452b902afa0dba72f1f9
joselivia <josephmutunga39@gmail.com>
2025-11-13 11:07:51 +0300

    Updates

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,4 +1,4 @@
-//export const baseURL = "https://politrackafrica.co.ke";
-export const baseURL = "http://localhost:8082";
+export const baseURL = "https://politrack.africa";
+//export const baseURL = "http://localhost:8082";
 export const apiURL = `${baseURL}/api`;
 export const baseURL2="http://localhost:8081";
